f73e7930f22dc276d8893bfdce56c0de57abd05f
Carl Coryell-Martin <pair+ccoryell@pivotallabs.com>
2010-09-08 11:24:21 +0800

    tweaked the logrotation stuff very slightly.

--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -7,7 +7,7 @@ remote_file "/etc/logrotate.d/nginx" do
   source "nginx.logrotate"
   owner "root"
   group "root"
-  mode 0644
+  mode 0755
   backup false  
   action :create
 end
@@ -16,7 +16,7 @@ remote_file "/etc/logrotate.d/application-logs" do
   source "application-logs.logrotate"
   owner "root"
   group "root"
-  mode 0644
+  mode 0755
   backup false
   action :create
 end
