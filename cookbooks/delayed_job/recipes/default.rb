8d3c7986c85f607fcf867d161f79fc36a30b4c92
Derrick Ko & Nate Clark <pair+derrick+nate@pivotallabs.com>
2010-09-21 16:00:04 +0800

    cleaned up new monit script for DelayedJob

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -5,7 +5,7 @@
 
 if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
   node[:applications].each do |app_name,data|
-  
+
     # determine the number of workers to run based on instance size
     if node[:instance_role] == 'solo'
       worker_count = 1
@@ -14,26 +14,24 @@ if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:na
       when 'm1.small': worker_count = 2
       when 'c1.medium': worker_count = 4
       when 'c1.xlarge': worker_count = 8
-      else 
+      else
         worker_count = 2
       end
     end
-    
-    worker_count.times do |count|
-      template "/etc/monit.d/delayed_job#{count+1}.#{app_name}.monitrc" do
-        source "dj.monitrc.erb"
-        owner "root"
-        group "root"
-        mode 0644
-        variables({
-          :app_name => app_name,
-          :user => node[:owner_name],
-          :worker_name => "delayed_job#{count+1}",
-          :framework_env => node[:environment][:framework_env]
-        })
-      end
+
+    template "/etc/monit.d/delayed_job.#{app_name}.monitrc" do
+      source "dj.monitrc.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables({
+        :app_name => app_name,
+        :user => node[:owner_name],
+        :worker_count => worker_count,
+        :framework_env => node[:environment][:framework_env]
+      })
     end
-    
+
     execute "monit-reload-restart" do
        command "sleep 30 && monit reload"
        action :run
